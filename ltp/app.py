112aae97c000c3e05a4dacb66bd9bfbeb88b596c
Shawn Lower <shawn@shawnlower.com>
2018-02-18 11:45:32 -0500

    Update config and use app factory to make testing easier

--- ltp/app.py
+++ ltp/app.py
@@ -2,26 +2,35 @@ import logging.config
 import os
 
 from flask import Flask, Blueprint
+from flask_sqlalchemy import SQLAlchemy
 
 # Local imports
 import settings
 from api.restplus import api
 from api.endpoints.healthcheck import ns as healthcheck_namespace
 from api.endpoints.activities import ns as activities_namespace
+from database.models import db
 
 logging.config.fileConfig('ltp/logging.cfg')
-
-app = Flask(__name__)
 log = logging.getLogger(__name__)
 
-def configure_app(flask_app):
-    # Load base defaults
-    flask_app.config.from_object('settings.BaseConfig')
-    if 'APP_CONFIG' in os.environ:
-        flask_app.config.from_envvar('APP_CONFIG')
+def create_app(config=None, skip_defaults=False):
+    """
+    app factory
+    If config is not passed, env is inspected for 'APP_CONFIG'. If not present,
+    the base defaults from settings.BaseConfig apply (local server, etc)
+    """
 
-def initialize_app(flask_app):
-    configure_app(flask_app)
+    app = Flask(__name__)
+    if not skip_defaults:
+        app.config.from_object('settings.BaseConfig')
+
+    if config:
+        app.config.from_object(config)
+    elif 'APP_CONFIG' in os.environ:
+        app.config.from_envvar('APP_CONFIG')
+
+    db.init_app(app)
 
     blueprint = Blueprint('api', __name__, url_prefix='/api')
     api.add_namespace(healthcheck_namespace)
@@ -29,8 +38,10 @@ def initialize_app(flask_app):
     api.init_app(blueprint)
     app.register_blueprint(blueprint)
 
+    return app
+
 def main():
-    initialize_app(app)
+    app = create_app()
     log.info('>>> Starting development server at http://{}/api/'.format(app.config['SERVER_NAME']))
     app.run()
 
--- ltp/settings.py
+++ ltp/settings.py
@@ -2,6 +2,11 @@ class BaseConfig(object):
     # Flask settings
     SERVER_NAME = 'localhost:8888'
     DEBUG = True  # Do not use debug mode in production
+    TESTING = True
+
+    # SQLAlchemy settings
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # Flask-Restplus settings
     RESTPLUS_SWAGGER_UI_DOC_EXPANSION = 'list'
