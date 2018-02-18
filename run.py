a6fb5270aabf2684bbbd2ce3b9be2edf2481a24d
Shawn Lower <shawn@shawnlower.com>
2018-02-18 18:30:34 -0500

    fix invocation syntax for create_app()

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ log = logging.getLogger(__name__)
 from ltp import app
 
 if __name__ == '__main__':
-    ltp_app = app.create_app()
+    ltp_app = app.create_app(__name__)
     log.info('>>> Starting development server at http://{}/api/'.format(ltp_app.config['SERVER_NAME']))
     ltp_app.run()
 
