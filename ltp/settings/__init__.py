dcafeab3b102d7b5ea7280bdf3d095170a8d1733
Shawn Lower <shawn@shawnlower.com>
2018-03-04 03:27:19 -0500

    Move configuration into ini / ConfigParser format. Add methods to generate Flask config objects. Add arg parsing

---
+++ ltp/settings/__init__.py
@@ -0,0 +1,83 @@
+from configparser import ConfigParser
+import logging
+import os
+from pprint import pprint, pformat
+
+log = logging.getLogger(__name__)
+logging.config.fileConfig('ltp/logging.cfg')
+
+DEFAULT_CONFIG = os.path.join(os.path.dirname(__file__), 'config.ini')
+
+class Config(object):
+
+    def __init__(self, file=None, skip_defaults=False, env=None):
+        """
+        Sets up a config object for use with Flask using an ini file as the
+        source. For non-core/flask settings, the config option is constructed
+        from the section name and the item, e.g.:
+
+            [sqlalchemy]
+            database_uri = ':memory:'
+                
+        Is accessed as:
+
+            config = Config()
+            config.SQLALCHEMY_DATABASE_URI
+
+        Flask settings and anything else in the [core] section is left as-is
+
+        :param str file: INI file containing settings (e.g. for prod/qa)
+        :param bool skip_defaults: Don't load config.ini from settings package
+        :param str env: Use a specific environment (e.g. testing)
+        """
+        self._config = dict()
+
+        if not skip_defaults:
+            self.update(DEFAULT_CONFIG)
+            if env:
+                self.update(DEFAULT_CONFIG, env=env)
+
+        if file:
+            self.update(file)
+            if env:
+                self.update(file, env=env)
+
+        log.debug("Config initialized with: \n{}".format(pformat(self._config)))
+
+    def update(self, file, env=None):
+        cp = ConfigParser()
+        if not cp.read(file):
+            raise IOError("Unable to load settings from {}".format(file))
+
+        # Find relevant sections. If we've specified an environment (env)
+        # then we want all sections [section:env] as well as core.
+        # If not, we want the inverse.
+
+        config = {}
+        for section in cp.sections():
+            s_name = section.partition(':')[0]
+            s_env = section.partition(':')[2]
+            if env and s_env == env:
+                config[s_name] = dict(cp[section])
+            if not env and not s_env:
+                config[section] = dict(cp[section])
+
+        # Merge contents
+        for section in config:
+            for item in config[section]:
+                if section == 'core':
+                    # Core / flask settings:
+                    key = item.upper()
+                else:
+                    # Other settings:
+                    key = section.upper() + '_' + item.upper()
+
+                self.set(key, config[section][item])
+
+    def set(self, key, value):
+        """
+        Setter to track updates in dict (e.g. for logging)
+        """
+        self._config[key] = value
+        setattr(self, key, value)
+
--- run.py
+++ run.py
@@ -1,10 +1,37 @@
+#!/usr/bin/env python
+
+import argparse
 import logging
+import os
+
 log = logging.getLogger(__name__)
 
 from ltp import app
+from ltp import settings
+from ltp.settings import Config
 
-if __name__ == '__main__':
-    ltp_app = app.create_app(__name__)
+def main():
+    parser = argparse.ArgumentParser(description='Run Flask interactively.')
+    parser.add_argument('--config', help='Config file with application '\
+                        'settings (default: {})'.format(settings.DEFAULT_CONFIG))
+    parser.add_argument('-e', '--environment', help='Set the environment '\
+            '(e.g. testing). This uses the config overrides (core:testing). '\
+            ' [NOTE]: You should NOT use this to separate prod/dev in the'\
+            ' same file!')
+    args = parser.parse_args()
+
+    config_file = None
+    if args.config:
+        config_file = args.config
+    elif 'LTP_CONFIG' in os.environ:
+        config_file = os.environ['LTP_CONFIG']
+
+    config = Config(file=config_file, env=args.environment)
+
+    ltp_app = app.create_app(__name__, config)
     log.info('>>> Starting development server at http://{}/api/'.format(ltp_app.config['SERVER_NAME']))
     ltp_app.run()
+
+if __name__ == '__main__':
+    main()
 
