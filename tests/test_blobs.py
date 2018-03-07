a803b4c5edb74fa3325b35816702b3097aad602e
Shawn Lower <shawn@shawnlower.com>
2018-03-06 22:45:17 -0500

    add list of packages to install for development (e.g. pip install -r requirements-dev.txt)

--- tests/test_blobs.py
+++ tests/test_blobs.py
@@ -18,9 +18,6 @@ class TestBlobs():
             "x-upload-content-length": 8,
             "x-upload-content-hash":
                 '916f0027a575074ce72a331777c3478d6513f786a591bd892da1a577bf2335f9',
-        },
-        'data': {
-            'file': (BytesIO(b'test data'), 'test.txt')
         }
     }
 
@@ -44,9 +41,10 @@ class TestBlobs():
         """
         path = "/api/blobs/"
 
-        response = client.post(path, 
-                        headers = self.BLOB_GOOD_DATA['headers'],
-                        data = self.BLOB_GOOD_DATA['data'])
+        data = {'file': (BytesIO(b'test data'), 'test.txt')}
+        headers = self.BLOB_GOOD_DATA['headers']
+
+        response = client.post(path, data=data, headers=headers)
 
         wrapped(response, testresult, request)
 
@@ -67,4 +65,15 @@ class TestBlobs():
         Creating a blob without the correct headers should fail
         """
 
-        assert "Test Not implemented" == '  :-(  '
+        path = "/api/blobs/"
+
+        data = {'file': (BytesIO(b'test data'), 'test.txt')}
+        headers = self.BLOB_GOOD_DATA['headers']
+        headers['x-upload-content-hash'] = 'XXX BAD HASH XXX'
+
+        response = client.post(path, data=data, headers=headers)
+
+        wrapped(response, testresult, request)
+
+        status_code = response.status_code
+        assert status_code == 201
