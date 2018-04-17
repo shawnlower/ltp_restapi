1544e20376b02da0b3b4953361f5a914d80a8c67
Shawn Lower <shawn@shawnlower.com>
2018-04-16 23:14:08 -0400

    Update SERVER_NAME when port is specified.

--- run.py
+++ run.py
@@ -10,6 +10,7 @@ from flask import Flask
 import argparse
 import logging
 import os
+import re
 import sys
 
 log = logging.getLogger(__name__)
@@ -18,7 +19,7 @@ log = logging.getLogger(__name__)
 def main():
     parser = argparse.ArgumentParser(description='Run Flask interactively.')
     parser.add_argument('-p', '--port', help='Port number to listen on.',
-                        type=int, default=8888)
+                        type=int, default=None)
     parser.add_argument('--config', help='Config file with app settings '
                         '(default: {})'.format(settings.DEFAULT_CONFIG))
     parser.add_argument('-e', '--environment', help='Set the environment '
@@ -32,11 +33,23 @@ def main():
         config_file = args.config
 
     config = Config(file=config_file, env=args.environment)
+
+    # Ensure port is overridden
+    if args.port:
+        server_name = config.SERVER_NAME
+        if ':' in server_name:
+            (host, _, _) = re.split(':([0-9]+)', server_name)
+        else:
+            host = server_name
+        config.SERVER_NAME = f"{host}:{args.port}"
+        log.debug("Using {} for server name.".format(
+            config.SERVER_NAME))
+
     app = create_app(__name__, config)
 
     log.info('>>> Starting development server at http://{}/api/'.format(
         app.config['SERVER_NAME']))
-    return app.run(port=args.port)
+    return app.run()
 
 
 if __name__ == '__main__':
